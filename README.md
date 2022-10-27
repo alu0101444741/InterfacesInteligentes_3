5fefee2dbb700b7ce82c267f62e9fab5efda2009
Roberto Carrazana Pernía <alu0101444741@ull.edu.es>
2022-10-27 21:06:42 +0100

    Commit final

--- README.md
+++ README.md
@@ -17,6 +17,12 @@ Esto se logra mediante la función 'LookAt' y finalmente 'transform.forward'.
 ![](https://github.com/alu0101444741/InterfacesInteligentes_3/blob/main/ScriptsGIFs/ObjectBBehaviour.gif)
 
 ## ObjectCBehaviour
-Una colisión del jugador con un objeto de tipo C, provocará que todos los objetos de tipo A salten y cambien su color por otro aleatorio.
+La aproximación del jugador a un objeto de tipo C, provocará dos reacciones:
+1- Todos los objetos de tipo A salten y cambien su color por otro aleatorio.
 El salto se logra mediante el método 'AddForce', aplicando una fuerza de tipo impulso (ForceMode.Impulse) sobre el eje Y.
+
+2- Todos los objetos de tipo B girarán para orientarse hacia un objetivo (cilindro).
 ![](https://github.com/alu0101444741/InterfacesInteligentes_3/blob/main/ScriptsGIFs/ObjectCBehaviour.gif)
+
+Funcionamiento de Debug.DrawRay.
+![](https://github.com/alu0101444741/InterfacesInteligentes_3/blob/main/ScriptsGIFs/DrawRay.gif)
--- code/ObjectCBehaviour.cs
+++ code/ObjectCBehaviour.cs
@@ -3,31 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectCBehaviour : MonoBehaviour {
+  private GameObject player;
   private GameObject[] objectsA;
   private GameObject[] objectsB;
+  private GameObject objectsBTarget;
+  private int proximity_sensitivity = 1;
+  private float rotationSpeed = 1.0f;
   void Start(){
+    this.player = GameObject.Find("Cube_Player");
     this.objectsA = GameObject.FindGameObjectsWithTag("TypeA");
     this.objectsB = GameObject.FindGameObjectsWithTag("TypeB");
+    this.objectsBTarget = GameObject.Find("Target");
   }
 
-  // Update is called once per frame
   void Update(){
-      
-  }
-
-  void OnCollisionEnter(Collision collision) {
-    if (collision.gameObject.name == "Cube_Player") {      
+    // If player gets closer enough
+    if(checkPlayerIsNear()) {
       for (int i = 0; i < this.objectsA.Length; ++i) {
         if (this.objectsA[i].GetComponent<ObjectABehaviour>().isGrounded) {
           this.objectsA[i].GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
           this.objectsA[i].GetComponent<Renderer>().material.color = Random.ColorHSV();
-        }        
+          this.objectsA[i].GetComponent<ObjectABehaviour>().isGrounded = false;
+        }               
       }
 
-      /*for (int i = 0; i < this.objectsB.Length; ++i) {
-        this.objectsA[i].transform.LookAt(this.objectC.transform.position);
-        this.objectsA[i].transform.position += this.objectsA[i].transform.forward;
-      }     */
+      Transform targetTransform = this.objectsBTarget.GetComponent<Transform>();
+
+      for (int i = 0; i < this.objectsB.Length; ++i) {
+        Transform objectTransform = this.objectsB[i].GetComponent<Transform>();
+
+        Vector3 targetDirection = targetTransform.localPosition - objectTransform.localPosition;
+        float singleStep = this.rotationSpeed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(objectTransform.forward, targetDirection, singleStep, 0.0f);
+
+        Debug.DrawRay(objectTransform.position, newDirection, Color.red, 1, false);
+        
+        // *******************************  Debug  *******************************
+        /*string objectPosition = this.objectsB[i].name +
+                                "(X:" + objectTransform.localPosition.x.ToString() +
+                                ", Z:" +  objectTransform.localPosition.z.ToString() + ")";
+        string targetPosition = objectsBTarget.name +
+                                "(X:" + targetTransform.localPosition.x.ToString() +
+                                ", Z:" +  targetTransform.localPosition.z.ToString() + ")";
+
+        Debug.Log("Drawing a line from " + objectPosition + " to " + targetPosition);*/
+        // *******************************         *******************************
+        
+        objectTransform.localRotation = Quaternion.LookRotation(newDirection);        
+      }
     }
+  }
+
+  private bool checkPlayerIsNear() {    
+    if (Mathf.Abs(player.transform.position.x - this.transform.position.x) <= this.proximity_sensitivity) {
+      if (Mathf.Abs(player.transform.position.z - this.transform.position.z) <= this.proximity_sensitivity) {
+        return(true);
+      }
+    }
+    return(false);
   }
 }
